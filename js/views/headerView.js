84a2718b70c3037cf21dc9822cf42ae61c6e1b12
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-02-20 20:31:39 +0100

    prepare to embed header to headerView

--- js/views/headerView.js
+++ js/views/headerView.js
@@ -1,19 +1,21 @@
 class HeaderView {
-  _parentElement = document.querySelector(".header-container");
   _renderNav = ``;
   _renderBack = ``;
   _renderTutorial = ``;
 
   headerRender(header, back, tutorial) {
     if (header)
-      this._parentElement.insertAdjacentHTML("afterbegin", this._renderNav);
+      document
+        .querySelector(".header-container")
+        .insertAdjacentHTML("afterbegin", this._renderNav);
     if (back)
-      this._parentElement.insertAdjacentHTML("afterbegin", this._renderBack);
+      document
+        .querySelector(".header-buttons")
+        .insertAdjacentHTML("afterbegin", this._renderBack);
     if (tutorial)
-      this._parentElement.insertAdjacentHTML(
-        "afterbegin",
-        this._renderTutorial
-      );
+      document
+        .querySelector(".header-buttons")
+        .insertAdjacentHTML("afterbegin", this._renderTutorial);
   }
 }
 
