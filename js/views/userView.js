4144a2f3463dd5ec64f58401f459c001b66430fb
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-03-10 19:12:09 +0100

    last changes to layout

--- js/views/userView.js
+++ js/views/userView.js
@@ -7,8 +7,10 @@ class UserView extends View {
   _headerConfig = [`User`, false, false, true];
 
   _HTML() {
-    return `        <div class="avatar-name-container">
-    <div class="avatar"></div>
+    return `        
+    <div class="avatar-name-experience-container">
+    <div class="avatar-name-container">
+    <div class="avatar avatar-man"></div>
     <div class="name-edit-container">
       <h3 class="user-name">Andrew</h3>
       <div class="edit-user edit-user-name"></div>
@@ -18,15 +20,17 @@ class UserView extends View {
     <button
       class="btn btn-navy btn-33 dropdown-experience-current-container"
     >
-      <h4 class="xperience-active-btn">beginner</h4>
+      <h4 class="experience-active-btn">beginner</h4>
       <div class="dropdown-arrow"></div>
     </button>
-    <ul class="dropdown-options-list">
+    <ul class="dropdown-options-list hidden">
       <!-- hidden -->
       <li class="dropdown-options-item">beginner</li>
       <li class="dropdown-options-item">intermediate</li>
     </ul>
   </div>
+  </div>
+  <div class="parameters-meter-bmi-container">
   <div class="user-parameters-container">
     <h4 class="user-parameters">Man 175 cm 89kg</h4>
     <div class="edit-user edit-user-parameters"></div>
@@ -36,7 +40,8 @@ class UserView extends View {
     <div class="bmi-meter-arrow"></div>
   </div>
   <h3 class="bmi-number">BMI: 20,1</h3>
-  <h4 class="bmi-label">Underweight</h4>`;
+  <h4 class="bmi-label">Underweight</h4>
+  </div>`;
   }
 }
 
