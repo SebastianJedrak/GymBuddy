77bda67cfc1ee23fa951821b5b4d5f86f6ce9238
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-03-13 18:55:49 +0100

    prepared registrationView

---
+++ js/views/registerView.js
@@ -0,0 +1,11 @@
+import View from "./view.js";
+
+class RegisterView extends View {
+  _parentElement = document.querySelector(".main-content");
+  _backgroundType = "gradient";
+  _navigator = false;
+  _headerConfig = [false, true, false, false];
+  _HTML() {
+    return ` `;
+  }
+}
