ea31503d9ee02ede8a2cff0c4ed82e78cfc915fd
Sebastian Jędrak <115486676+SebastianJedrak@users.noreply.github.com>
2024-09-02 13:21:42 +0200

    Update config.js
    vercel intrgration

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
-import { key } from "./API_key.js";
+// import { key } from "./API_key.js";
 
 export const options = {
   method: "GET",
-  headers: { "X-Api-Key": key },
+  headers: { "X-Api-Key": process.env.API_KEY },
   contentType: "application/json",
   origin: "*",
 };
